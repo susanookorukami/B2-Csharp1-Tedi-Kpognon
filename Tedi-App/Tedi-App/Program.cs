e75c5c0bc5819fdfccf778fdbbee2cdc49288bed
tedi kpognon <tedi.kpognon@epsi.fr>
2020-10-05 13:50:58 +0200

    premier commit

---
+++ Tedi-App/Tedi-App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tedi_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
