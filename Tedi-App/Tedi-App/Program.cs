5ce107d08a0a300caa638a1dc4d88f19a9075b62
tedi kpognon <tedi.kpognon@epsi.fr>
2020-10-05 16:57:05 +0200

    toto

--- Tedi-App/Tedi-App/Program.cs
+++ Tedi-App/Tedi-App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Tedi_App
 {
@@ -6,7 +7,112 @@ namespace Tedi_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Ville> Listville = new List<Ville>();
+            
+            while (true)
+            {
+                string choixUtilisateur = MenuVille();
+
+                if (choixUtilisateur == "1")
+                {
+                    Ville V = CreateVille();
+                    Listville.Add(V);
+                }
+                else if (choixUtilisateur == "2")
+                {
+                    affiche(Listville);
+                }
+                else if (choixUtilisateur == "3")
+                {
+                    affiche(Listville);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Je n'ai pas compris");
+                }
+            }
+            Console.ReadKey();
+
+        }
+        public static string MenuVille()
+        {
+            Console.WriteLine("que voulez vous faire?");
+            Console.WriteLine("1.Cree une nouvelle ville");
+            Console.WriteLine("2.afficher l'ensemble des villes");
+            Console.WriteLine("3.Afficher le nombre total d'habitant");
+            string MenuVille = Console.ReadLine();
+            return MenuVille;
+        }
+        public static void affiche(List<Ville> Ville)
+        {
+            foreach (Ville V in Ville)
+            {
+                string message;
+                message = Buildmessage(V);
+                Console.WriteLine(message);
+            }
+        }
+
+        public static Ville CreateVille()
+        {
+            //intentiation de locataire
+            Ville V = new Ville();
+
+            //demande de prenom
+            V.Nom = NbLettre("Nom :");
+            //demande de place
+            V.CodePostal = NbEntier("Code Postal :");
+            //temps de location
+            V.Habitants = NbEntier("Habitant :");
+            //crée le message
+            //affiche le message
+            
+            return V;
+        }
+
+        public static string Buildmessage(Ville V)
+        {
+            string result;
+
+            result = "--------------------"+
+                "nom : "+ V.Nom + " Code Postal : " + V.CodePostal + 
+                " Nombre d'habitant : " + V.Habitants + 
+                "----------------------------------";
+            return result;
+        }
+
+        public static string NbLettre(string message)
+        {
+            Console.WriteLine(message);
+             string Nom;
+             Nom = Console.ReadLine();
+            int intville;
+                if (int.TryParse(Nom, out intville))
+                {
+                    Console.WriteLine("la saisie est invalide");
+                    Nom = Console.ReadLine();
+                 }
+                    return Nom;
+           
+        }
+        public static int NbEntier(string message)
+        {
+            Console.WriteLine(message);
+            string CodePostal;
+            CodePostal = Console.ReadLine();
+            int intValue;
+            while (!int.TryParse(CodePostal, out intValue)&&intValue >=0)
+            {
+                Console.WriteLine("la saisie est invalide");
+                CodePostal = Console.ReadLine();
+            }
+            return intValue;
         }
     }
 }
+    
+
+    
+
+    
---
+++ Tedi-App/Tedi-App/Ville.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tedi_App
+{
+    class Ville
+    {
+        public string Nom;
+        public int CodePostal;
+        public int Habitants;
+    }
+}
